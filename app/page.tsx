60d28f4ae59d4d9381a6f951eb94d4cbce088eba
Dom <domeccleston@gmail.com>
2024-01-09 22:48:06 +0000

    tweak

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ function YearGrid({ currentDay }: { currentDay: number }) {
 
 	const days = Array.from(Array(totalDays).keys());
 	return (
-		<div className="border rounded-lg border-gray-400 pl-3 md:px-3 pt-3 mt-10 pb-3 md:pb-0">
+		<div className="border-0 md:border rounded-lg border-gray-400 pl-3 md:px-3 pt-3 mt-10 pb-3 md:pb-0">
 			<div className="flex flex-row md:flex-col flex-wrap max-w-[120px] md:h-[120px] md:min-w-[848px] md:max-w-full items-center gap-[4px]">
 				{days.map((_, i) => {
 					console.log(i < currentDay);
@@ -64,7 +64,7 @@ export default function Home() {
 			<h1 className="text-3xl md:text-7xl mt-20 md:mt-0 text-white font-medium tracking-[-0.035em]">
 				2024 is {progress}% complete.
 			</h1>
-			<h2 className="text-xl md:text-2xl mt-10 text-gray-400">
+			<h2 className="text-xl md:text-2xl mt-10 text-slate-400 tracking-tight">
 				It&apos;s week {week}, day {dayOfWeek} of 2024.
 			</h2>
 			<YearGrid currentDay={currentDay} />
